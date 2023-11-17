2619c33200d3e8126ff5f50c1e6e99b4db8cfb72
sahil952988 <sahilacc000@gmail.com>
2023-11-17 17:40:49 +0545

    changes

--- src/pages/CalculatorDesign.js
+++ src/pages/CalculatorDesign.js
@@ -35,22 +35,22 @@ const CalculatorDesign = () => {
       </div>
 
       <div className="grid grid-cols-4 gap-3 mr-3 mt-5 ml-3">
-        <button className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">C</button>
-        <button className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-white text-[30px]" ><i class="fa-solid fa-delete-left"></i></button>
+        <button onClick={clear} id="clear" className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">C</button>
+        <button onClick={backspace} id="backspace" className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-white text-[30px]" ><i class="fa-solid fa-delete-left"></i></button>
         <button className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-white text-[30px]"><i class="fa-solid fa-divide"></i></button>
-        <button className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px] ">*</button>
-        <button className="w-[70px] h-[50px] text-[#0 bg-gray-700 rounded-lg shadow-2xl0FFFF] text-[30px]">7</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">8</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">9</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">-</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">4</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">5</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">6</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">+</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">1</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">2</button>
-        <button className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">3</button>
-        <button className="w-[70px] h-[115px] bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">=</button>
+        <button name="*" onClick={handleClick} className="w-[70px] h-[50px] bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px] ">*</button>
+        <button name="7" onClick={handleClick} className="w-[70px] h-[50px] text-[#0 bg-gray-700 rounded-lg shadow-2xl0FFFF] text-[30px]">7</button>
+        <button name="8" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">8</button>
+        <button name="9" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">9</button>
+        <button name="-" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">-</button>
+        <button name="4" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">4</button>
+        <button name="5" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">5</button>
+        <button name="6" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">6</button>
+        <button name="+" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">+</button>
+        <button name="1" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">1</button>
+        <button name="2" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">2</button>
+        <button name="3" onClick={handleClick} className="w-[70px] h-[50px]  bg-gray-700 rounded-lg shadow-2xl text-[#00FFFF] text-[30px]">3</button>
+        <button onClick={calculate} id="result" className="w-[70px] h-[115px] bg-gray-700 rounded-lg shadow-2xl text-[#e65f78] text-[30px]">=</button>
       </div>
 
 
